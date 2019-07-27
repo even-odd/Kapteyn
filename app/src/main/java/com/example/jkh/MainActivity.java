3391748781a1d4f1201216f67e4e950b045122e7
RMS <danilereshenko@gmail.com>
2019-07-27 20:51:51 +0300

    server connected

--- app/src/main/java/com/example/jkh/MainActivity.java
+++ app/src/main/java/com/example/jkh/MainActivity.java
@@ -12,60 +12,57 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.Call;
+import retrofit2.http.POST;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyApp";
+
+    public class Expression {
+
+         long a;
+         long b;
+         long c;
+
+}
+
+    public interface ServerApi {
+
+        @GET("index.php/?a=2&b=5")
+        Call<Expression> expression();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
     }
-
-    public class Message {
-
-        private long id;
-        private long time;
-        private String text;
-        private String image;
-
-        // getters and setters
-
-    }
-    public interface MessagesApi {
-
-        @GET("messages1.json")
-        Call<List<Message>> messages();
-    }
-
-
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-    MessagesApi messagesApi = retrofit.create(MessagesApi.class);
-
-
-
-
-
-
 
     @Override
     protected void onResume() {
-        super.onResume();
-        Call<List<Message>> messages = messagesApi.messages();
-        messages.enqueue(new Callback<List<Message>>() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://u0773476.cp.regruhosting.ru/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ServerApi messagesApi = retrofit.create(ServerApi.class);
+
+        Call<Expression> messages = messagesApi.expression();
+
+        messages.enqueue(new Callback<Expression>() {
             @Override
-            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
-                Log.d(TAG,"response " + response.body().size());
+            public void onResponse(Call<Expression> call, Response<Expression> response) {
+                Log.i(TAG,"response " + response.body().c);
             }
 
             @Override
-            public void onFailure(Call<List<Message>> call, Throwable t) {
-
+            public void onFailure(Call<Expression> call, Throwable t) {
+                Log.i(TAG,"failure " + t);
             }
         });
+
+        super.onResume();
     }
 }
